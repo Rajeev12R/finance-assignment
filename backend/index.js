5bf9ec187e5cf30f730e409a97845dab4ed17640
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-04-02 17:16:43 +0530

    feat(middleware): Added middleware for checking auth and roles

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,20 @@ dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
 
+//Routes
+import authRoute from "./auth/auth.route.js"
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use('/api/auth', authRoute);
+
+
 
 app.get('/health', (req, res) => {
     res.send("Health is OK. Backend running.")
 })
-
 app.listen((port), () => {
     connectDB();
     console.log(`Server Started at PORT: http://localhost:${port}`);
