e62ebc145ef51802f294014f4efb4c66c2dd37b9
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-04-04 02:28:51 +0530

    feat: Added Rate limiter

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import authRoutes from "./src/modules/auth/auth.route.js"
 import recordRoutes from "./src/modules/financialRecord/financialRecord.routes.js"
 import dashboardRoutes from "./src/modules/dashboard/dashboard.routes.js"
 import userRoutes from "./src/modules/user/user.routes.js"
+import { rateLimit } from "express-rate-limit"
 import errorHandler from "./src/middlewares/error.middleware.js"
 
 const port = process.env.PORT || 3000;
@@ -17,6 +18,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, 
+    limit: 50, 
+    standardHeaders: true, 
+    legacyHeaders: false, 
+    message: "Too many requests from this IP, please try again after 5 minutes"
+});
+
+app.use("/api/", limiter);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/records', recordRoutes);
